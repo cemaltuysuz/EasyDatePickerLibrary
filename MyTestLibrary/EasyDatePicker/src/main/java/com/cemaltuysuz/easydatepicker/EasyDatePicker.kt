06d146ade947fd9c849f6a913d4763d935304e6b
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-09-22 21:55:19 +0300

    EasyDatePicker.kt some functions and comments

--- MyTestLibrary/EasyDatePicker/src/main/java/com/cemaltuysuz/easydatepicker/EasyDatePicker.kt
+++ MyTestLibrary/EasyDatePicker/src/main/java/com/cemaltuysuz/easydatepicker/EasyDatePicker.kt
@@ -8,12 +8,35 @@ package com.cemaltuysuz.easydatepicker
 
 import android.app.Activity
 
-class EasyDatePicker (activity:Activity) {
+/**
+ * In order to create a Date Picker dialog, we need to get activity information from the user.
+ * */
+class EasyDatePicker (val activity:Activity) {
 
-    // Custom Style
+    /**
+     * The user can create his own style if he wishes.
+     * Until then, the default style is considered selected.
+     * @see R.style.DefaultDatePickerStyle
+     * */
     private var style:Int = R.style.DefaultDatePickerStyle
-    // Activity
-    private var activity: Activity = activity
-    // onDateSelectListener interface for
+
+    /**
+     * When the user completes the date selection process,
+     * we send her the selected date information with interface.
+     * @see OnDateSelectListener
+     * */
     private var listener: OnDateSelectListener? = null
+
+    /**
+     * User can change default style
+     * */
+    fun setDatePickerStyle(style:Int):EasyDatePicker{
+        this.style = style
+        return this
+    }
+
+    fun setListener(listener: OnDateSelectListener):EasyDatePicker{
+        this.listener = listener
+        return this
+    }
 }
